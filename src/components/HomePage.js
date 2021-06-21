5052a65ac5e8ca24d39f49ae21e097b956db40df
github username <github email address>
2021-06-21 12:48:22 -0400

    Add working WordsAPI fetch request

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,4 +1,24 @@
+import { useEffect } from "react"
+
 function HomePage() {
+    const myKey='your key here'
+    
+    useEffect(() => {
+
+        fetch('https://wordsapiv1.p.rapidapi.com/words/?random=true',
+            {
+	        method: "GET",
+	        headers: {
+		        'x-rapidapi-key': myKey,
+		        'x-rapidapi-host': "wordsapiv1.p.rapidapi.com"
+                }
+            })
+            .then(resp => resp.json())
+            .then(console.log)
+
+    }, [])
+    
+
     return (
         <div>
             <h1>Cruddy Words</h1>
